e0e639be75949cb4922894ca1bd950c31363d583
Nguyen Quyet Thang <thangnq111@ghtk.co>
2023-02-08 18:22:17 +0700

    locust monitor

---
+++ app/locustfile.py
@@ -0,0 +1,14 @@
+from locust import HttpUser, TaskSet, task
+
+class Message(HttpUser):
+    @task
+    def get_alpha(self):
+        self.client.post('/alpha', data={'message': 1})
+
+    @task
+    def get_beta(self):
+        self.client.post('/beta', data={'message': 1})
+
+    @task
+    def get_gamma(self):
+        self.client.post('/gamma', data={'message': 1})
--- app/main.py
+++ app/main.py
@@ -6,10 +6,10 @@ from controllers.webhooks_controller import webhooks_routes
 app = Flask(__name__)
 
 # Routes
-app.register_blueprint(webhooks_routes, url_prefix='/webhooks')
+app.register_blueprint(webhooks_routes)
 
 API_HANDLE_HOST = os.getenv('API_HANDLE_HOST', '0.0.0.0')
-API_HANDLE_PORT = os.getenv('API_HANDLE_PORT', 5000)
+API_HANDLE_PORT = os.getenv('API_HANDLE_PORTT', 8000)
 
 if __name__ == '__main__':
     app.run(host=API_HANDLE_HOST, port=API_HANDLE_PORT, debug=True, threaded=False, processes=3)
