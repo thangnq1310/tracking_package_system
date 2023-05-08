68691f23868ea4fa3666ef517e722045e27e9c55
Ubuntu <azureuser@thangnq.jrzqmynprq4eti02hnjy30ftid.hx.internal.cloudapp.net>
2023-05-08 11:08:08 +0000

    update api

--- server/routes/partner.route.js
+++ server/routes/partner.route.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const partnerController = require("../controllers/partner.controller");
 
-router.get("/alpha", partnerController.webhookAlpha);
+router.post("/alpha", partnerController.webhookAlpha);
 
 router.post("/beta", partnerController.webhookBeta);
 
